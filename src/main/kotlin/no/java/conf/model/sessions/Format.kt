2f463cca5e78bfded1f563914e005e31b231d328
Chris Searle <chris@chrissearle.org>
2024-09-09 08:00:36 +0200

    Add missing formats and log unknown codes

--- src/main/kotlin/no/java/conf/model/sessions/Format.kt
+++ src/main/kotlin/no/java/conf/model/sessions/Format.kt
@@ -1,6 +1,9 @@
 package no.java.conf.model.sessions
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.Serializable
+
+private val logger = KotlinLogging.logger {}
 
 @Serializable
 enum class Format(
@@ -9,10 +12,13 @@ enum class Format(
     PRESENTATION("presentation"),
     LIGHTNING_TALK("lightning-talk"),
     WORKSHOP("workshop"),
+    PANEL("panel"),
+    BOF("bof"),
     UNKNOWN("unknown"),
     ;
 
     companion object {
-        fun fromCode(code: String?) = entries.find { it.code == code } ?: UNKNOWN
+        fun fromCode(code: String?) =
+            entries.find { it.code == code } ?: UNKNOWN.also { logger.warn { "Unknown format $code" } }
     }
 }
--- src/main/kotlin/no/java/conf/model/sessions/Language.kt
+++ src/main/kotlin/no/java/conf/model/sessions/Language.kt
@@ -1,6 +1,9 @@
 package no.java.conf.model.sessions
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.Serializable
+
+private val logger = KotlinLogging.logger {}
 
 @Serializable
 enum class Language(
@@ -12,6 +15,7 @@ enum class Language(
     ;
 
     companion object {
-        fun fromCode(code: String?) = entries.find { it.code == code } ?: UNKNOWN
+        fun fromCode(code: String?) =
+            entries.find { it.code == code } ?: UNKNOWN.also { logger.warn { "Unknown language $code" } }
     }
 }
