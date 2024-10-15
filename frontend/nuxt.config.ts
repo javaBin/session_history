0fbbeb8cb4925b94ee4842b5f749015d8dbfa7dc
Chris Searle <chris@chrissearle.org>
2024-10-15 19:05:48 +0200

    Get frontend built by gradle

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,3 +1,18 @@
+const routeRulesProxy = () => {
+    if (process.env.NODE_ENV === 'development') {
+        return {
+            '/api/**': {
+                proxy: {
+                    to: 'http://127.0.0.1:8080/api/**'
+                }
+            }
+
+        }
+    } else {
+        return {}
+    }
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
@@ -8,13 +23,7 @@ export default defineNuxtConfig({
         enabled: true
     },
     modules: ['vuetify-nuxt-module', '@nuxt/scripts', '@nuxt/icon'],
-    routeRules: {
-        '/api/**': {
-            proxy: {
-                to: 'http://127.0.0.1:8080/api/**'
-            }
-        }
-    },
+    routeRules: routeRulesProxy(),
     icon: {
         clientBundle: {
             scan: true,
